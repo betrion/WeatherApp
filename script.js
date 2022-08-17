c45d0565e0b986e1e0771d3a81fb51856bf4f037
betrion <forteee@gmail.com>
2022-08-17 14:49:49 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const apikey = "Qn2sqIb33mPOEeH4Is1s5X0h9MOdBOA7";
+const searchTerm = "cats";
+const img = document.querySelector("img");
+const searchButton = document.querySelector("button");
+const inputBox = document.querySelector("input");
+
+function giphy(search = searchTerm) {
+  fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${apikey}&s=${search}`)
+    .then((response) => {
+      // createP(response);
+      console.log(response);
+      response.json().then((json) => {
+        img.src = `${json.data.images.original.url}`;
+    //   });
+    })
+    .catch((err) => console.log("shiit" + err));
+}
+giphy();
+
+searchButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  giphy(inputBox.value);
+});
